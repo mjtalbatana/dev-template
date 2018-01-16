b2241810a1959d5e21e61a0b7b7c3cdd02656637
Michael James Albatana <mjtalbatana@gmail.com>
2018-01-16 16:36:54 +0800

    added php templates, for further update and code consolidation

---
+++ php/head.php
@@ -0,0 +1,14 @@
+<?php
+echo
+"
+<head><!-- ==================================================================================================================== -->
+  <meta charset=\"utf-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+  <!-- <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0\"> -->
+  <meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">
+  <title>TEMPLATE</title>
+  <link rel=\"icon\" href=\"https://raw.githubusercontent.com/mjtalbatana/profile-logo/master/mjta-v01.ico\" type=\"image/x-icon\">
+  <link rel=\"stylesheet\" href=\"assets/scss/style.css\" type=\"text/css\">
+</head><!-- =================================================================================================================== -->
+"
+?>
---
+++ php/signature.php
@@ -0,0 +1,11 @@
+<?php
+echo
+"
+<!-- HTML5 + PHP7 Template version 1.6 20180116 ============-->
+<!-- Developer: Michael James Turiano Albatana              -->
+<!-- Email: mjtalbatana@gmail.com                           -->
+<!-- Github: https://github.com/mjtalbatana                 -->
+<!-- LinkedIn: https://www.linkedin.com/in/mjtalbatana      -->
+<!-- END OF HTML FILE ======================================-->
+"
+?>
