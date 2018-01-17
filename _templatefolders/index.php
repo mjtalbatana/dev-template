15ff6bb13b0e2e321526ef6b0388e1819d0ab988
Michael James Albatana <mjtalbatana@gmail.com>
2018-01-17 21:28:59 +0800

    major overhaul of devtemplates

---
+++ _templatefolders/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>TITLE</title>
+</head>
+<?php include 'assets/php/head.php';?>
+
+<body>
+
+<?php include 'assets/php/scripts.php';?>
+</body>
+
+</html>
---
+++ php/scripts.php
@@ -0,0 +1,7 @@
+<?php
+echo
+"
+<script src=\"assets/js/script.js\"></script>
+<?php include 'assets/php/basic.php';?>
+";
+?>
