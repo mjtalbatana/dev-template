0d64d713026c6cd5e92596f115b63de18a62b909
Michael James Albatana <mjtalbatana@gmail.com>
2018-01-22 16:31:03 +0800

    consolidated the template

---
+++ _template-php/index.php
@@ -0,0 +1,17 @@
+<?php
+  $v_htmlsubtitle = '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require 'assets/php/head.php'?>
+<body>
+<?php require 'assets/php/tooltipbar.php'?>
+<?php require 'assets/php/navheader.php'?>
+<main>
+
+
+</main>
+<?php require 'assets/php/footer.php'?>
+</body>
+</html>
+<?php require 'assets/php/signature.php'?>
--- php/scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo
-"
-<script src=\"assets/js/script.js\"></script>
-<?php include 'assets/php/basic.php';?>
-";
-?>
